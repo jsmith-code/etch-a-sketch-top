1a75d2dd009a76ec02f73127ec2ac346b95dd5cd
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-24 06:51:58 -0600

    Add generated 16x16 pixel grid
    Since the grid uses flexbox, pixels in it will scale to take up whatever
    space is available.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const canvasContainer = document.querySelector('#canvas-container');
+
+for (let row = 0; row < 16; row++) {
+    const pixelRow = document.createElement('div');
+    pixelRow.classList.add('pixel-row');
+    canvasContainer.appendChild(pixelRow);
+    for (let col = 0; col < 16; col++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixelRow.appendChild(pixel);
+    }
+}
