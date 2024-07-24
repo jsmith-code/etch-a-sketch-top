cb1234a7479b7f4ed2b0e1abab2c81c7be78b780
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-24 07:12:17 -0600

    Refactor grid generation into function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-const canvasContainer = document.querySelector('#canvas-container');
+function generateGrid() {
+    const canvasContainer = document.querySelector('#canvas-container');
 
-for (let row = 0; row < 16; row++) {
-    const pixelRow = document.createElement('div');
-    pixelRow.classList.add('pixel-row');
-    canvasContainer.appendChild(pixelRow);
-    for (let col = 0; col < 16; col++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixelRow.appendChild(pixel);
+    for (let row = 0; row < 16; row++) {
+        const pixelRow = document.createElement('div');
+        pixelRow.classList.add('pixel-row');
+        canvasContainer.appendChild(pixelRow);
+        
+        for (let col = 0; col < 16; col++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixelRow.appendChild(pixel);
+        }
     }
 }
+
+generateGrid();
