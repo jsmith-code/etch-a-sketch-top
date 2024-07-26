988e8be907b5e4318c1eb08a40b826b6c318bd0c
Jacob Smith <129335957+IAmSlowlyLearningToCode@users.noreply.github.com>
2024-07-26 07:44:52 -0600

    Add button to create new grid
    Prompts user for grid size and if input is valid, generates a new grid.
     - Submitting empty input cancels grid creation
     - Decimal inputs are rounded to the nearest integer

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+const newGridBtn = document.querySelector('#new-grid-btn');
+
+newGridBtn.addEventListener('click', () => {
+    while (true) {
+        const input = prompt('Enter new grid size (must be between 1 - 100)');
+        if (!input) {
+            return;
+        }
+
+        const size = Math.round(Number(input));
+        if (!isNaN(size) && size >= 1 && size <= 100) {
+            generateGrid(size);
+            return;
+        }
+    }
+});
+
 function generateGrid(size) {
     const canvasContainer = document.querySelector('#canvas-container');
 
@@ -22,7 +39,10 @@ function generateGrid(size) {
 function createPixel() {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
-    
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = '#333333';
+    });
+
     return pixel;
 }
 
