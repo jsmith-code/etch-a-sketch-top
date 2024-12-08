741017d919d4fa856cb4b100c7621c0be0c034e1
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2024-12-08 09:40:30 -0700

    Move eventListener declarations to bottom of code
    Helps keep things organized and ensures eventListeners are only added
    after everything has been declared

--- scripts.js
+++ scripts.js
@@ -4,33 +4,6 @@ const colorPicker = document.querySelector('#color-picker');
 
 let brushColor = '#000000';
 
-colorPicker.addEventListener('change', (e) => {
-    const color = e.target.value;
-    brushColor = color;
-})
-
-newGridBtn.addEventListener('click', () => {
-    const size = getIntBetween(1, 100);
-    if (isNaN(size)) {
-        return;
-    }
-    generateGrid(size);
-});
-
-// Set pixel color and opacity on mouseover
-pixelGrid.addEventListener('mouseover', (e) => {
-    if (!e.buttons) return;
-    const pixel = e.target;
-    pixel.style.backgroundColor = brushColor;
-});
-
-pixelGrid.addEventListener('mousedown', (e) => {
-    if (!e.buttons) return;
-    console.log(e.target)
-    const pixel = e.target;
-    pixel.style.backgroundColor = brushColor;
-});
-
 function generateGrid(size) {
     const gridExists = pixelGrid.hasChildNodes();
     if (gridExists) {
@@ -64,5 +37,32 @@ function getIntBetween(min, max) {
     } while (isNaN(input) || input < min || input > max);
     return Math.round(input);
 }
+
+colorPicker.addEventListener('change', (e) => {
+    const color = e.target.value;
+    brushColor = color;
+})
+
+newGridBtn.addEventListener('click', () => {
+    const size = getIntBetween(1, 100);
+    if (isNaN(size)) {
+        return;
+    }
+    generateGrid(size);
+});
+
+// Set pixel color and opacity on mouseover
+pixelGrid.addEventListener('mouseover', (e) => {
+    if (!e.buttons) return;
+    const pixel = e.target;
+    pixel.style.backgroundColor = brushColor;
+});
+
+pixelGrid.addEventListener('mousedown', (e) => {
+    if (!e.buttons) return;
+    console.log(e.target)
+    const pixel = e.target;
+    pixel.style.backgroundColor = brushColor;
+});
 
 generateGrid(16);
